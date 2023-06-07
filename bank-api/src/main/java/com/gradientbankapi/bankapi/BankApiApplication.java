b57cd03fe2d37f5f75ed067c267eb14ebcb6b905
jestrada07 <jaimiestrada007@gmail.com>
2023-06-07 14:14:22 -0400

    Initial commit

---
+++ bank-api/src/main/java/com/gradientbankapi/bankapi/BankApiApplication.java
@@ -0,0 +1,13 @@
+package com.gradientbankapi.bankapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApiApplication.class, args);
+	}
+
+}
