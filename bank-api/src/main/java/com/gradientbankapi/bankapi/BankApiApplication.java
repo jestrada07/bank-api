bd2f62d40f450b57b678de0ca673ef0952543bb9
jestrada07 <jaimiestrada007@gmail.com>
2023-06-14 10:37:06 -0400

    idk

--- bank-api/src/main/java/com/gradientbankapi/bankapi/BankApiApplication.java
+++ bank-api/src/main/java/com/gradientbankapi/bankapi/BankApiApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-git public class BankApiApplication {
+public class BankApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BankApiApplication.class, args);
